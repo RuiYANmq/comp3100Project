3676fd8b3b990c712d0d261fc2a6edaca5f39991
Rui Yan <yanrui6668@gmail.com>
2020-05-15 05:49:18 +1000

    best-fit
    best fit changed

--- bf/BFClient.java
+++ bf/BFClient.java
@@ -1,3 +1,4 @@
+package bf;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,6 +11,10 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.List;
+import java.util.Hashtable;
+import java.util.Collections;
+import java.util.Comparator;
 
 class Job {
   private int subTime;
@@ -37,7 +42,7 @@ class Job {
     //System.out.printf("job %d: estTime:%d core:%d memory:%d disk:%d\n", this.id, this.estTime, this.core, this.memory, this.disk);
   }
   public String getRESC() {
-    String ret = String.format("RESC All %d %d %d", this.core, this.memory, this.disk);
+    String ret = String.format("RESC Capable %d %d %d", this.core, this.memory, this.disk);
     return ret;
   }
   public int getID() {
@@ -46,9 +51,9 @@ class Job {
   public Boolean available(int core, int mem, int disk) {
     return (this.core <= core && this.memory <= mem && this.disk <= disk);
   }
-  public int fit(int core, int memory, int disk) {
-    return core - this.core;
-  }
+    public int getCore() {
+        return this.core;
+    }
 }
 
 class Server {
@@ -69,19 +74,14 @@ class Server {
     this.memory = Integer.parseInt(splitStr[5]);
     this.disk = Integer.parseInt(splitStr[6]);
   }
-  public Server(String type, int core, int mem, int disk) {
-    this.type = type;
+  public void update(int avail, int core, int mem, int disk) {
+    this.availTime = avail;
     this.core = core;
     this.memory = mem;
     this.disk = disk;
   }
-  public void update(String recv) {
-    String[] splitStr = recv.trim().split(" ");
-    this.state = Integer.parseInt(splitStr[2]);
-    this.availTime = Integer.parseInt(splitStr[3]);
-  }
   public Boolean available(Job job) {
-    if (this.state > 3)//Unavailable
+    if (this.state == 4)//Unavailable
       return false;
     return job.available(this.core, this.memory, this.disk);
   }
@@ -95,7 +95,7 @@ class Server {
     return String.format("%s %d %d %d %d %d %d", this.type, this.id, this.state, this.availTime, this.core, this.memory, this.disk);
   }
   public int fit(Job job) {
-    return job.fit(this.core, this.memory, this.disk);
+    return this.core - job.getCore();
   }
   public int getAvailTime() {
     return this.availTime;
@@ -103,15 +103,102 @@ class Server {
   public String getType() {
     return this.type;
   }
+    public int getState() {
+        return this.state;
+    }
 }
     
+class Type {
+    private String name;
+    private int limit;
+    private int core;
+    private int memory;
+    private int disk;
+    private List<Server> servers = new ArrayList<Server>();
+    public Type(String name, int limit, int core, int memory, int disk) {
+        this.name = name;
+        this.limit = limit;
+        this.core = core;
+        this.memory = memory;
+        this.disk = disk;
+    }
+    public void addServer(Server s) {
+        this.servers.add(s);
+    }
+    public String getType() {
+        return this.name;
+    }
+    public int computeFitness(Job job)  {
+        return this.core - job.getCore();
+    }
+    public Server getServer(int id) {
+        return this.servers.get(id);
+    }
+    public List<Server> getServers() {
+        return this.servers;
+    }
+    public int getServerSize() {
+        return this.servers.size();
+    }
+    public int getCore() {
+        return this.core;
+    }
+    public Boolean available(Job job) {
+        return job.available(this.core, this.memory, this.disk);
+    }
+    public Boolean exist(int id) {
+      if(this.servers.size() > id)
+        return true;
+      return false;
+    }
+}
+ 
+class SystemInfo {
+    private List<Type> types = new ArrayList<Type>();
+    private Hashtable<String, Type> typeMap = new Hashtable<>();
+    public SystemInfo() {}
+    public void addType(Type type) {
+        this.types.add(type);
+        this.typeMap.put(type.getType(), type);
+    }
+    public void sortType() {
+        Collections.sort(this.types, new Comparator<Type>() {
+            @Override
+            public int compare(Type t1, Type t2) {
+                return t1.getCore() - t2.getCore();
+            }
+        });
+    }
+    public void updateServer(String recv) {
+        String[] splitStr = recv.split(" ");
+        String type = splitStr[0];
+        int id = Integer.parseInt(splitStr[1]);
+        Type typeObj = this.typeMap.get(type);
+        if(typeObj.exist(id)) {
+          Server s = this.typeMap.get(type).getServer(id);
+          int avail = Integer.parseInt(splitStr[3]);
+          int core = Integer.parseInt(splitStr[4]);
+          int mem = Integer.parseInt(splitStr[5]);
+          int disk = Integer.parseInt(splitStr[6]);
+          s.update(avail, core, mem, disk);
+        } else {
+          typeObj.addServer(new Server(recv));
+        }
+    }
+    public Type getType(String type) {
+        return this.typeMap.get(type);
+    }
+    public List<Type> getTypeList() {
+        return this.types;
+    }
+}
+
 public class BFClient {
     private String bestType = null;
     private OutputStream out;
     private InputStream in;
-    private ArrayList<Server> servers = new ArrayList<>();
-    private ArrayList<Server> systemInfo = new ArrayList<>();
     private Boolean debug = false;
+    private SystemInfo system = new SystemInfo();
 
     private void sendMsg(String str) throws Exception {
         if(this.debug)
@@ -135,7 +222,7 @@ public class BFClient {
             System.err.printf("expect: %s, recv err: %s.\n", str, msg);
             System.exit(1);
         }
-        return msg;
+        return msg.trim();
     }
 
     private void cntj(Job job) throws Exception {
@@ -143,16 +230,21 @@ public class BFClient {
       Boolean find = false;
       int bestFit = Integer.MAX_VALUE;
       int minAvail = Integer.MAX_VALUE;
-      Server select = null;
+      int wstFit = Integer.MAX_VALUE;
 
-      for(Server server: this.servers) {
+      Server select = null;
+    Server selectW = null;
+
+    for(Type type: this.system.getTypeList()) {
+      for(Server server: type.getServers()) {
         send = server.getCNTJ();
         this.sendMsg(send);
         recv = this.recvMsg();
         //if(job.getID()==19)
          //System.out.printf("server: %s, avail:%b, fit:%d\n", server.showAll(), server.available(job), server.fit(job));
-        if(server.available(job) && recv.equals("0")) {
-          int fit = server.fit(job);
+        int state = server.getState();
+        int fit = server.fit(job);
+        if(server.available(job)) {
           int availTime = server.getAvailTime();
           if (fit < bestFit || (bestFit == fit && availTime < minAvail)) {
             bestFit = fit;
@@ -160,34 +252,32 @@ public class BFClient {
             select = server;
             find = true;
           }
+        //} else if ((state % 2 == 1) && type.available(job)) {
+        } else if (type.available(job)) {
+            fit = type.computeFitness(job);
+            if (fit < wstFit) {
+                wstFit = fit;
+                selectW = server;
+            }
         }
       }
+    }
+
       if(find) {
         send = String.format("SCHD %d %s", job.getID(), select.show()); 
         this.sendMsg(send);
         this.recvMsg("OK");
       } else {
-        for(Server server: this.systemInfo) {
-          if(server.available(job)) {
-            int fit = server.fit(job);
-            if(fit < bestFit) {
-              bestFit = fit;
-              select = server;
-            }
-          }
-        }
-            
-        send = String.format("SCHD %d %s 0", job.getID(), select.getType()); 
+        send = String.format("SCHD %d %s", job.getID(), selectW.show()); 
         this.sendMsg(send);
         this.recvMsg("OK");
       }
     }
 
-    private void resc(Job job) throws Exception {
+    private void resc(Job job, Boolean first) throws Exception {
         String send, recv;
         this.recvMsg("DATA");
         this.sendMsg("OK");
-        this.servers.clear();
         int index = 0;
         while (true) {
             recv = this.recvMsg();
@@ -195,10 +285,14 @@ public class BFClient {
                 this.cntj(job);
                 return;
             }
-              this.servers.add(new Server(recv));
+            this.system.updateServer(recv);
             index++;
             this.sendMsg("OK");
         }
+    }
+
+    private void resc(Job job) throws Exception {
+        this.resc(job, false);
     }
 
     private void sendJobs() throws Exception {
@@ -213,7 +307,7 @@ public class BFClient {
         Job job = new Job(recv);
         send = job.getRESC();
         this.sendMsg(send);
-        this.resc(job);
+        this.resc(job, first);
         if(first)
           first = false;
       }
@@ -227,18 +321,18 @@ public class BFClient {
         NodeList sList = d.getElementsByTagName("server");
         for (int i = 0;i < sList.getLength();i++) {
             Element e = (Element)sList.item(i);
+            int limit = Integer.parseInt(e.getAttribute("limit"));
             int core = Integer.parseInt(e.getAttribute("coreCount"));
             int mem = Integer.parseInt(e.getAttribute("memory"));
             int disk = Integer.parseInt(e.getAttribute("disk"));
             String type = e.getAttribute("type");
-            this.systemInfo.add(new Server(type, core, mem, disk));
+            this.system.addType(new Type(type, limit, core, mem, disk));
         }
     }
 
 
     public void run() {
         try {
-            this.parse();
             Socket socket=new Socket("localhost", 50000);
             out = socket.getOutputStream();
             in = socket.getInputStream();
@@ -248,6 +342,7 @@ public class BFClient {
             String user = map.get("USERNAME");
             this.sendMsg(String.format("AUTH %s", user));
             this.recvMsg("OK");
+            this.parse();
 
             this.sendJobs();
             this.sendMsg("QUIT");
---
+++ bf/TestBF.java
@@ -0,0 +1,7 @@
+import bf.BFClient;
+class TestBF {
+    public static void main(String[] args) {
+        BFClient c = new BFClient();
+        c.run();
+    }
+}
